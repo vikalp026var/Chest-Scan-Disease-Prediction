8f6de20f0ed5bd81f72d6dd9e9e5361fbc175b12
vikalp026var <vikalp026varshney@gmail.com>
2024-10-06 22:52:05 +0530

    Updated code

---
+++ main.py
@@ -0,0 +1,12 @@
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.data_ingestion import DataIngestion
+
+
+try:
+    config = ConfigurationManager()
+    data_ingestion_config = config.get_data_ingestion_config()
+    data_ingestion = DataIngestion(config=data_ingestion_config)
+    data_ingestion.download_file()
+    data_ingestion.extract_zip_file()
+except Exception as e:
+    raise e
